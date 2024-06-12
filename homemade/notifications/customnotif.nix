fd7772a7d3ba072b25bbdf331ca71ee5d4235761
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-06-12 14:59:15 -0400

    test (Failed) - 2024-06-12|14:59:14

--- homemade/notifications/customnotif.nix
+++ homemade/notifications/customnotif.nix
@@ -1,8 +1,9 @@
-{lib, pkgs, python311Packages } :
-
 # Technically, I should be using buildPythonPackage or buildPythonApplication, but I don't want to setup the required file to make a pip package, soo....
 
-stdenv.mkDerivation rec {
+let
+	pkgs = import <nixpkgs> {};
+in
+pkgs.stdenv.mkDerivation rec {
 	name = "customnotif";
 	propogatedBuildInputs = [
 		(pkgs.python311.withPackages (python-pkgs: with python-pkgs; [
