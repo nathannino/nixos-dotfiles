c2f300edccf855ab241693514da5c5934de38f39
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-07-21 11:42:20 -0400

    switch (Failed) - 2024-07-21|11:42:19

--- modules/nixos/default-packages.nix
+++ modules/nixos/default-packages.nix
@@ -7,26 +7,35 @@
         programs.firefox.enable = true;
         services.flatpak.enable = true;
 
+  	# List packages installed in system profile. To search, run:
+  	# $ nix search wget
         environment.systemPackages = with pkgs; [
-            filezilla
-            librewolf # TODO : https://nixos.wiki/wiki/Librewolf#Configuration
-            obs-studio
-            nh
-            tree
-            prismlauncher
-            python3
-            htop
-            expect
-            jq
-            unar
-            nil
-            mpv
-            # dunst (Replaced by custom package, see qtile.nix)
-            libnotify
-	    nix-output-monitor
-	    nvd
-	    qpwgraph
-	    rssguard
+    		vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    		neovim
+    		wget
+    		vesktop
+    		git
+    		alacritty
+    		bat
+            	filezilla
+            	librewolf # TODO : https://nixos.wiki/wiki/Librewolf#Configuration
+            	obs-studio
+            	nh
+            	tree
+            	prismlauncher
+            	python3
+            	htop
+            	expect
+            	jq
+            	unar
+            	nil
+            	mpv
+            	# dunst (Replaced by custom package, see qtile.nix)
+            	libnotify
+	    	nix-output-monitor
+	    	nvd
+	    	qpwgraph
+	    	rssguard
         ];
     };
 }
