317281d71f80d97b7aea475a16c235e1704917f4
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-07-21 11:44:11 -0400

    switch (Failed) - 2024-07-21|11:44:10

--- modules/nixos/default-config.nix
+++ modules/nixos/default-config.nix
@@ -66,6 +66,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  xdg.portal.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
