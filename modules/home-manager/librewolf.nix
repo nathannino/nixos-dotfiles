10115b0650294a5f2e862a36dea8cc76dd74f7be
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-07-22 16:19:21 -0400

    switch (Success) - 2024-07-22|16:19:20

--- modules/home-manager/librewolf.nix
+++ modules/home-manager/librewolf.nix
@@ -10,9 +10,8 @@
             "general.autoScroll" = true;
             "security.OCSP.require" = false; # TODO : Check if this is really needed. It might not be too bad to just toggle it when necessary...
             "network.cookie.lifetimePolicy" = 0; #TODO : Very not sure if I actually want thsi...
-	    #"widget.use-xdg-desktop-portal" = true;
-	    #"widget.use-xdg-desktop-portal.file-picker" = 1;
-	    #"widget.use-xdg-desktop-portal.mime-handler" = 1;
+	    "widget.use-xdg-desktop-portal.file-picker" = 1;
+	    "widget.use-xdg-desktop-portal.mime-handler" = 1;
 	    #"widget.use-xdg-desktop-portal.location" = 1;
 	    #"widget.use-xdg-desktop-portal.open-uri" = 1;
 	    #"widget.use-xdg-desktop-portal.settings" = 1;
