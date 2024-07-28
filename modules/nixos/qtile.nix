4c6e1137a0f84f7ee364dc2cdca48ae44ea8669d
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-07-27 22:25:04 -0400

    test (Failed) - 2024-07-27|22:25:03

--- modules/nixos/qtile.nix
+++ modules/nixos/qtile.nix
@@ -1,5 +1,9 @@
 {lib, config, pkgs, ...}:
 
+    	imports = [
+		./wlr.nix
+    	];
+
 let
   cfg = config.qtilemodule;
 in
@@ -12,9 +16,6 @@ in
     #  };
     #};
     config = {
-    	imports = [
-		./wlr.nix
-    	];
       #services.xserver.windowManager.qtile = { # This literally just installs qtile what???? At best, also adds a config file.
       #  enable = true;
       # # configFile = ./../../hosts/nvidiadesktop/qtilecfg.py; # Doesn't work =(. Check home.nix instead
