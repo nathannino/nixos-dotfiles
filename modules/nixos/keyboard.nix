259e1eaf70586885a4fc045797ee649ceac1bc09
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-09-05 09:51:25 -0400

    test (Success) - 2024-09-05|09:51:24

--- modules/nixos/keyboard.nix
+++ modules/nixos/keyboard.nix
@@ -6,18 +6,29 @@ in
 {
     options.keyboardmodule = {
 	layout = lib.mkOption {
-		type = lib.types.string;
+		type = lib.types.str;
 		default = "us";
 	};
+	variant = lib.mkOption {
+		type = lib.types.str;
+		default = "";
+	};
 	options = lib.mkOption {
-		type = lib.types.string;
+		type = lib.types.str;
 		default = "";
 	};
     };
     config = {
 	services.xserver = {
 		xkb.layout = cfg.layout;
+		xkb.variant = cfg.variant;
 		xkb.options = cfg.options;
+	};
+
+	environment.variables = {
+		XKB_DEFAULT_LAYOUT = cfg.layout;
+		XKB_DEFAULT_VARIANT = cfg.variant;
+		XKB_DEFAULT_OPTIONS = cfg.options;
 	};
     };
 }
