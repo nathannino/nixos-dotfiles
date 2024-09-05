c2d6d2efaeb74bef4d0e046f47b19c95ffa57cb4
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2024-09-05 12:10:25 -0400

    fix tab spacing in keyboard.nix

--- modules/nixos/keyboard.nix
+++ modules/nixos/keyboard.nix
@@ -4,31 +4,31 @@
 let cfg = config.keyboardmodule;
 in
 {
-    options.keyboardmodule = {
-	layout = lib.mkOption {
-		type = lib.types.str;
-		default = "us";
+	options.keyboardmodule = {
+		layout = lib.mkOption {
+			type = lib.types.str;
+			default = "us";
+		};
+		variant = lib.mkOption {
+			type = lib.types.str;
+			default = "";
+		};
+		options = lib.mkOption {
+			type = lib.types.str;
+			default = "";
+		};
 	};
-	variant = lib.mkOption {
-		type = lib.types.str;
-		default = "";
-	};
-	options = lib.mkOption {
-		type = lib.types.str;
-		default = "";
-	};
-    };
-    config = {
-	services.xserver = {
-		xkb.layout = cfg.layout;
-		xkb.variant = cfg.variant;
-		xkb.options = cfg.options;
-	};
+	config = {
+		services.xserver = {
+			xkb.layout = cfg.layout;
+			xkb.variant = cfg.variant;
+			xkb.options = cfg.options;
+		};
 
-	environment.variables = {
-		XKB_DEFAULT_LAYOUT = cfg.layout;
-		XKB_DEFAULT_VARIANT = cfg.variant;
-		XKB_DEFAULT_OPTIONS = cfg.options;
+		environment.variables = {
+			XKB_DEFAULT_LAYOUT = cfg.layout;
+			XKB_DEFAULT_VARIANT = cfg.variant;
+			XKB_DEFAULT_OPTIONS = cfg.options;
+		};
 	};
-    };
 }
