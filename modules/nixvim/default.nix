df3fe31944d0ab763521ef5b426d67b225bddaee
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-01-05 17:41:14 -0500

    switch (Failed) - 2025-01-05|17:41:11

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -6,7 +6,16 @@
 		viAlias = true;
 		vimAlias = true;
 
-		clipboard.register = "unnamedplus";
-		clipboard.providers.xsel.enable = true;
+		clipboard = {
+			register = "unnamedplus";
+			providers.xsel.enable = true;
+			providers.wl-copy.enable = true;d.
+		};
+
+		colorschemes = {
+			onedark = {
+				enable = true;
+			};
+		};
 	};
 }
