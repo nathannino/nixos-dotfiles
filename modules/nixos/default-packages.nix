6614b6375009f8289d5eba5b6de8839b1e0c0945
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-01-05 19:45:48 -0500

    boot (Success) - 2025-01-05|19:45:45

--- modules/nixos/default-packages.nix
+++ modules/nixos/default-packages.nix
@@ -14,7 +14,6 @@
         environment.systemPackages = with pkgs; [
     		# vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     		# neovim # check the nixvim module
-		lunarvim
     		wget
     		vesktop
     		git
