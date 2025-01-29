2e09f962b0ec9a30a31e7878151d5b9e0c013320
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-01-29 13:08:24 -0500

    switch (Success) - 2025-01-29|13:08:17

--- modules/nixos/slack.nix
+++ modules/nixos/slack.nix
@@ -3,42 +3,23 @@
 {lib, config, pkgs, ...}:
 
 {
-    config = {
+	config = {
 
    	nixpkgs.overlays = [
-		(self: super: {
-			slack = super.slack.overrideAttrs(new: old: { # Fix qtile wayland issue with slack
-				#postInstall = let
-				#	desktopfile = ''
-				#		[Desktop Entry]
-				#		Name=Slack
-				#		StartupWMClass=Slack
-				#		Comment=Slack Desktop
-				#		GenericName=Slack Client for Linux
-				#		Exec=${pkgs.bashInteractive}/bin/sh -c "${pkgs.killall}/bin/killall slack ; $out/bin/slack -s %U"
-				#		Icon=slack
-				#		Type=Application
-				#		StartupNotify=true
-				#		Categories=GNOME;GTK;Network;InstantMessaging;
-				#		MimeType=x-scheme-handler/slack;
-				#	'';
-				#in (old.postInstall or "") + ''
-				#	echo "${desktopfile}" > $out/share/applications/slack.desktop
-				#	chmod 777 $out/share/applications/slack.desktop
-				#'';
+			(self: super: {
+				slack = super.slack.overrideAttrs(new: old: { # Fix qtile wayland issue with slack
+					postInstall = ''
+						substituteInPlace $out/share/applications/slack.desktop \
+        			--replace "$out/bin/slack" "${pkgs.bashInteractive}/bin/sh -c \"${pkgs.killall}/bin/killall slack ; $out/bin/slack -s %U\""
+					'';
+				});
+			})
+		];
 
-				postInstall = ''
-					substituteInPlace $out/share/applications/slack.desktop \
-        		--replace "$out/bin/slack" "${pkgs.bashInteractive}/bin/sh -c \"${pkgs.killall}/bin/killall slack ; $out/bin/slack -s %U\""
-				'';
-			});
-		})
-	];
-
-        environment.systemPackages = with pkgs; [
-            slack
-	    teams-for-linux # Insert swear word here
-        ];
-    };
+    environment.systemPackages = with pkgs; [
+      slack
+	  	teams-for-linux # Insert swear word here
+  	];
+  };
 }
 
