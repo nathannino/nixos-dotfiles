ab8dae77961b361c05608496fb6d45118624f18e
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-03-11 11:40:45 -0400

    boot (Success) - 2025-03-11|11:40:40

--- modules/nixos/arduino.nix
+++ modules/nixos/arduino.nix
@@ -4,15 +4,6 @@
 
 {
     config = {
-
-		nixpkgs.overlays = [
-			(self: super: {
-				arduino-ide = super.arduino-ide.overrideAttrs(new: old: {
-					nativeBuildInputs = old.nativeBuildInputs or [] ++ [ super.xorg.libxkbfile ];
-				});
-			})
-		];
-
   	# List packages installed in system profile. To search, run:
   	# $ nix search wget
         environment.systemPackages = with pkgs; [
