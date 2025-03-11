f4b06f6400d75f15de2ce53d6aaebf644046b257
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-03-11 15:15:03 -0400

    switch (Success) - 2025-03-11|15:14:59

--- modules/home-manager/arduino.nix
+++ modules/home-manager/arduino.nix
@@ -3,7 +3,7 @@
 let
 	arduino-appimage = pkgs.fetchurl {
 		url = "https://github.com/arduino/arduino-ide/releases/download/2.3.4/arduino-ide_2.3.4_Linux_64bit.AppImage";
-		sha256 = "ksrns5zaaqn8rv877l0lzl7mhjvkdxzq8lnz4s6lhn4jdybf99z";
+		sha256 = "0ksrns5zaaqn8rv877l0lzl7mhjvkdxzq8lnz4s6lhn4jdybf99z";
 	};
 in
 {
