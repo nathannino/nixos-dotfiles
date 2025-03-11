cc402eb82d9f95d4c0fb2890e5a2e93833bc5dbb
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-03-11 14:28:16 -0400

    switch (Failed) - 2025-03-11|14:28:13

--- modules/nixos/arduino.nix
+++ modules/nixos/arduino.nix
@@ -4,15 +4,17 @@
 
 {
 
-   	nixpkgs.overlays = [
-			(self: super: {
-				arduino-ide = super.arduino-ide.overrideAttrs(new: old: { # Fix qtile wayland issue with slack
-					propogatedBuildInputs = old.propogatedBuildInputs ++ [super.python3Packages.pyserial];
-				});
-			})
-		];
 
     config = {
+
+
+   		nixpkgs.overlays = [
+				(self: super: {
+					arduino-ide = super.arduino-ide.overrideAttrs(new: old: { # Fix qtile wayland issue with slack
+						propogatedBuildInputs = old.propogatedBuildInputs ++ [super.python3Packages.pyserial];
+					});
+				})
+			];
   	# List packages installed in system profile. To search, run:
   	# $ nix search wget
         environment.systemPackages = with pkgs; [
