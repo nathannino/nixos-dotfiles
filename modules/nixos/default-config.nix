601a32b5b25060c7e3c874f911beaf2ef7256b33
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-04-07 15:33:47 -0400

    switch (Success) - 2025-04-07|15:33:44

--- modules/nixos/default-config.nix
+++ modules/nixos/default-config.nix
@@ -12,7 +12,7 @@
          	extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
 				'';
 			settings.experimental-features = [ "nix-command" "flakes" ];
-		}
+		};
 
   # VERY TEMP ISSUE, REVERT WHEN FIXED
   boot.kernelPackages = pkgs.linuxPackages_latest;
