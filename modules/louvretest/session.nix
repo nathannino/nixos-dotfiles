7ddd746c24f2886e788cbb10150fa07df203cbb0
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-05-10 11:22:15 -0400

    boot (Success) - 2025-05-10|11:22:13

--- modules/louvretest/session.nix
+++ modules/louvretest/session.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
 	version = "0.1";
 	name = "LouvreTemplate";
 	src = ./files;
+	passthru.providedSessions = [ "impatientcomp" ];
 	installPhase = ''
 		mkdir -p $out/share/wayland-sessions/
 		cp * $out/share/wayland-sessions/
