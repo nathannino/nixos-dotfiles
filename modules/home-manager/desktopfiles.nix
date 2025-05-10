d1a3f5568766a051a056f7687eb14c168c6c46d3
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-05-10 00:57:40 -0400

    switch (Success) - 2025-05-10|00:57:37

--- modules/home-manager/desktopfiles.nix
+++ modules/home-manager/desktopfiles.nix
@@ -2,11 +2,12 @@
 {
 	xdg.desktopEntries = {
 		steamgamescope = {
+			type = "Application";
 			name = "Steam (Gamescope)";
 			genericName = "Steam Under Gamescope";
 			exec = "${pkgs.gamescope}/bin/gamescope -e -- steam";
 			terminal = false;
-			categories = [ "Applications" "Network" "Games"];
+			categories = ["Network" "Game"];
 		};
 	};
 }
